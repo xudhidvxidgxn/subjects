30889594e1679135d965368497397fbacfa79ece
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-23 22:46:33 +0900

    2주차 과제 - 3

--- 2주차 과제-3/week3.js
+++ 2주차 과제-3/week3.js
@@ -1,11 +1,20 @@
 function mergeObjects(obj1, obj2) {
   // 코드 작성
-  return { ...obj1, ...obj2 };
+  return { ...obj1, ...obj2 }; // 이건 임시입니다
 }
 
-const object1 = { a: "A", b: "B", c: "C" };
-const object2 = { b: "X", c: "Y", d: "Z" };
+let object1 = { a: "A", b: "B", c: "C" };
+let object2 = { b: "X", c: "Y", d: "Z" };
 
 console.log(mergeObjects(object1, object2)); // {a: 'A', b: 'X', c: 'Y', d: 'Z'} 출력
+
+function countLetters(string) {
+  //  return new Map([...string].map((key) => [key, [...string].filter((value) => value === key).length]));
+  return Object.fromEntries([...string].map((key) => [key, [...string].filter((value) => value === key).length]));
+}
+
+let str = "apple";
+
+console.log(countLetters(str)); // {a: 1, p: 2, l: 1, e: 1} 출력
 
 console.log(Object.prototype);
