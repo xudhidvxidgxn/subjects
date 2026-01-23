8fbc75bd77777663e3ca257536917bc637031830
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-23 21:34:35 +0900

    week3.js

---
+++ 2주차 과제-3/week3.js
@@ -0,0 +1,10 @@
+function mergeObjects(obj1, obj2) {
+  // 코드 작성
+}
+
+const object1 = { a: "A", b: "B", c: "C" };
+const object2 = { b: "X", c: "Y", d: "Z" };
+
+console.log(mergeObjects(object1, object2)); // {a: 'A', b: 'X', c: 'Y', d: 'Z'} 출력
+
+console.log(Symbol.prototype);
