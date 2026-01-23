094951a87853de852a824d429ad90e45bd372903
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-24 00:03:14 +0900

    2주차 과제 - 3

--- 2주차 과제-3/week3.js
+++ 2주차 과제-3/week3.js
@@ -69,4 +69,20 @@ let students = [
 
 console.log(filterByScore(students, 80)); // ["Alice", "Cathy"] 출력
 
+function filterByAverageScore(students, score) {
+  // 코드 작성
+  return Object.values(students)
+    .filter((value) => [...value["score"]].reduce((acc, cur) => acc + cur, 0) / value["score"].length >= score)
+    .map((key) => key["name"]);
+}
+
+students = [
+  { name: "Alice", score: [90, 60, 70, 100] },
+  { name: "Bob", score: [75, 35, 40, 60] },
+  { name: "Cathy", score: [90, 10, 20, 30] },
+  { name: "David", score: [70, 75, 85, 95] },
+];
+
+console.log(filterByAverageScore(students, 80)); // ["Alice", "David"] 출력
+
 console.log(Object.prototype);
