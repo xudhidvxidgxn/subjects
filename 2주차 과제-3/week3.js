47381533985b92bfbc3a0e02aef91293198ab455
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-23 21:50:45 +0900

    2주차 과제-3

--- 2주차 과제-3/week3.js
+++ 2주차 과제-3/week3.js
@@ -8,4 +8,13 @@ const object2 = { b: "X", c: "Y", d: "Z" };
 
 console.log(mergeObjects(object1, object2)); // {a: 'A', b: 'X', c: 'Y', d: 'Z'} 출력
 
+const arr = [1, 2, 3, 4];
+const a1 = arr.reduce((acc, cur, idx, arr) => {
+  console.log(`acc:${acc}, cur:${cur}, idx:${idx}`);
+  return acc + cur;
+}, 0); // 초기값 0
+console.log(a1);
+
+console.log(arr.reduce((acc, cur) => acc + cur));
+
 console.log(Object.prototype);
