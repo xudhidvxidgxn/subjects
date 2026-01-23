2723d0b4b3b6c75039814c979ed0b39a89dfc80a
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-23 15:48:45 +0900

    js 연구

--- 2주차 과제-3/week2-2.js
+++ 2주차 과제-3/week2-2.js
@@ -16,14 +16,26 @@ function insertElement(arr, index, value) {
     if (index >= arr.length) {
         return console.error(`현재 배열의 길이는 ${arr.length}로 ${index}은 입력 불가능합니다.`);
     }
-    const res = arr.slice(0, index);
-    res.push(value);
-    const res1 = arr.slice(index, arr.length)
+    else {
+        const res = arr.slice(0, index);
+        res.push(value);
+        const res1 = arr.slice(index, arr.length);
 
-    return res.concat(res1);
+        return res.concat(res1);
+    }
 }
 
 const nums = [1, 2, 3, 4, 5];
 
 console.log(insertElement(nums, 2, 6)); // [1, 2, 6, 3, 4, 5] 출력
 console.log(insertElement(nums, 10, 6)); // console.error("현재 배열의 길이는 5로 10은 입력 불가능합니다.")
+
+function removeElement(arr, index) {
+    // 코드 작성
+    const res = arr.filter((value) => value !== arr[index]);
+    return res;
+}
+
+const chars = ['A', 'B', 'C', 'D', 'E'];
+
+console.log(removeElement(chars, 3)); // ['A', 'B', 'C', 'E'] 출력
