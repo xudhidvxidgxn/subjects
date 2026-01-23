88c5b79d5e4aefdc692c56e2e2a7aba232d86c77
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-23 21:42:45 +0900

    2주차 과제-3

--- 2주차 과제-3/week3.js
+++ 2주차 과제-3/week3.js
@@ -1,5 +1,6 @@
 function mergeObjects(obj1, obj2) {
   // 코드 작성
+  console.log(...Object.keys(obj1));
 }
 
 const object1 = { a: "A", b: "B", c: "C" };
@@ -7,4 +8,4 @@ const object2 = { b: "X", c: "Y", d: "Z" };
 
 console.log(mergeObjects(object1, object2)); // {a: 'A', b: 'X', c: 'Y', d: 'Z'} 출력
 
-console.log(Symbol.prototype);
+console.log(Object.prototype);
