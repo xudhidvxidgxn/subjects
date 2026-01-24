52021df1eac2aacaf9320d9122707936a0ee5cf4
김민재 <ys2510103@y-y.hs.kr>
2026-01-24 10:54:04 +0900

    20260124

--- 기능반 특강/20260124/app.js
+++ 기능반 특강/20260124/app.js
@@ -6,7 +6,23 @@ css: 이쁘게 보일려고
     -> 살
 js: 
     -> 근육
+    
+js 파일은 맨 하단에
 */
-const button = document.querySelector("button");
+const button = document.querySelector("#default-btn");
+const buttonList = document.querySelectorAll(".btn");
 
 console.log(button);
+
+//스타일링
+//왜 쓰는가 : 특정 시점에 스타일을 바꾸고 싶을 때;
+button.style.width = "100px";
+button.style.height = "50px";
+
+//요소의 계층 위치 변경
+const first = document.querySelector(".first");
+
+first.appendChild(button); //추가
+first.removeChild(button); //삭제
+
+//이벤트 추가
