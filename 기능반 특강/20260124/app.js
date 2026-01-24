0e172b1ef0f3939b3e0264fc3842d9a014d770c6
김민재 <ys2510103@y-y.hs.kr>
2026-01-24 10:59:46 +0900

    DOM 제어의 기본기

--- 기능반 특강/20260124/app.js
+++ 기능반 특강/20260124/app.js
@@ -9,20 +9,34 @@ js:
     
 js 파일은 맨 하단에
 */
-const button = document.querySelector("#default-btn");
+const button = document.querySelector(".btn");
 const buttonList = document.querySelectorAll(".btn");
 
 console.log(button);
+console.log(buttonList);
 
 //스타일링
 //왜 쓰는가 : 특정 시점에 스타일을 바꾸고 싶을 때;
-button.style.width = "100px";
-button.style.height = "50px";
+//button.style.width = "100px";
+//button.style.height = "50px";
+buttonList.forEach((button) => {
+  button.style.width = "100px";
+  button.style.height = "50px";
+});
 
 //요소의 계층 위치 변경
 const first = document.querySelector(".first");
 
-first.appendChild(button); //추가
-first.removeChild(button); //삭제
+//first.appendChild(button); //추가
+//first.removeChild(button); //삭제
+
+button.textContent = "바뀜";
+
+const newButton = document.createElement("button");
+
+newButton.textContent = "aaa"; // 택스트 채우기
+newButton.classList.add("btn"); // 클래스 할당
+
+first.appendChild(newButton);
 
 //이벤트 추가
