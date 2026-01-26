de0fe16ab39ce149269e078e387f04ce93c52aba
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-26 14:18:28 +0900

    함수 수정

--- 3주자 과제-1/문제1/main.js
+++ 3주자 과제-1/문제1/main.js
@@ -2,10 +2,10 @@ const button = document.getElementById("addBtn");
 
 let num = 0;
 
-const addListItem = () => {
+function addListItem() {
   const list = document.getElementById("itemList");
   num++;
   list.insertAdjacentHTML("beforeend", `<li>아이템 ${num}</li>`);
-};
+}
 
 button.addEventListener("click", addListItem);
