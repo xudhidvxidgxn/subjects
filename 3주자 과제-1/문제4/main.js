79edc3337868a1a4fd6b1c3983197c4ef965a8ec
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-26 16:15:46 +0900

    3주차 과제-1 문제4

---
+++ 3주자 과제-1/문제4/main.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("analyzeBtn");
+const input = document.getElementById("numberInput");
+const display = document.getElementById("resultContainer");
+
+function analyzeNumber() {
+	// 코드 작성
+    
+}
