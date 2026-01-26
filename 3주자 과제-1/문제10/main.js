1696af289b0651c3fe4bcfcb71325f24dd51cf7f
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-26 19:26:34 +0900

    오타 삭제

--- 3주자 과제-1/문제10/main.js
+++
@@ -1,5 +0,0 @@
-const box = document.getElementById("custombox");
-box.addEventListener("contextmenu", (ev) => {
-    ev.preventDefault();
-    console.log(`Custom right-click action`);
-});
--- 3주자 과제-1/문제9/main.js
+++
@@ -1,7 +0,0 @@
-const subBtn = document.querySelector(`#myForm > button[type="submit"]`);
-subBtn.addEventListener("click", (ev) => {
-    ev.preventDefault();
-    const name = document.getElementById("username").value;
-    const pw = document.getElementById("password").value;
-    console.log(`username : ${name}\npassword : ${pw}`)
-});
