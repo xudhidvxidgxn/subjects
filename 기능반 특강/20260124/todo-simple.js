e7c679dec1f81cee435cfa41cff65bcb560fa9c2
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-26 14:35:38 +0900

    버그수정

--- 기능반 특강/20260124/todo-simple.js
+++ 기능반 특강/20260124/todo-simple.js
@@ -1,40 +1,40 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    const input = document.querySelector(".todo-input");
-    if (!(input.value === "")) {
-        //  const newTodo = document.createElement("div");
-        //  newTodo.textContent = input.value;
+  const input = document.querySelector(".todo-input");
+  if (!(input.value === "")) {
+    //  const newTodo = document.createElement("div");
+    //  newTodo.textContent = input.value;
 
-        //  document.querySelector("#todo-list").appendChild(newTodo);
+    //  document.querySelector("#todo-list").appendChild(newTodo);
 
-        // insertAdjacentHTML: 텍스트를 파싱해서 html로 추가한다.
-        // beforeend: 맨 뒤에 추가한다.
-        document.querySelector("#todo-list").insertAdjacentHTML(
-            "beforeend",
-            `<div class="todo">
+    // insertAdjacentHTML: 텍스트를 파싱해서 html로 추가한다.
+    // beforeend: 맨 뒤에 추가한다.
+    document.querySelector("#todo-list").insertAdjacentHTML(
+      "beforeend",
+      `<div class="todo">
             ${input.value}
-            <button class="todo-remove">삭제<button>
+            <button class="todo-remove">삭제</button>
         </div>
         `,
-        );
-        const removeBtn = document.querySelectorAll(".todo-remove");
-        removeBtn.forEach((value, index) => {
-            if (index === removeBtn.length - 1) {
-                value.addEventListener("click", (ev) => {
-                    //document.querySelector("#todo-list").removeChild(ev.target.closest("div"));
-                    document.querySelector("#todo-list").removeChild(ev.target.parentNode);
-                });
-            }
+    );
+    const removeBtn = document.querySelectorAll(".todo-remove");
+    removeBtn.forEach((value, index) => {
+      if (index === removeBtn.length - 1) {
+        value.addEventListener("click", (ev) => {
+          //document.querySelector("#todo-list").removeChild(ev.target.closest("div"));
+          document.querySelector("#todo-list").removeChild(ev.target.parentNode);
         });
-        //  document.querySelector(".todo-input").value = "";
-    }
+      }
+    });
+    //  document.querySelector(".todo-input").value = "";
+  }
 });
 
 const clearButton = document.querySelector(".clear");
 
 clearButton.addEventListener("click", () => {
-    document.querySelector(".todo-input").value = "";
+  document.querySelector(".todo-input").value = "";
 });
 
 // 3번
