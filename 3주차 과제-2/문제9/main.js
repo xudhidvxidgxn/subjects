10abd15ad710578f23fc7871e6ae2b45f054f781
김민재 <ys2510103@y-y.hs.kr>
2026-01-29 11:42:13 +0900

    3주차 과제-2

---
+++ 3주차 과제-2/문제9/main.js
@@ -0,0 +1,62 @@
+function getText1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Hello, ");
+        }, 1000);
+    });
+}
+
+function getText2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("World! ");
+        }, 500);
+    });
+}
+
+function getText3() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("This is a test.");
+        }, 1500);
+    });
+}
+
+// 코드 작성
+function mergeText1() {
+    return new Promise((resolve, reject) => {
+        let txt = ``;
+        getText1().then((res) => {
+            txt += res;
+            getText2().then((res) => {
+                txt += res;
+                getText3().then((res) => {
+                    txt += res;
+                    resolve(txt);
+                });
+            });
+        });
+    });
+}
+
+function mergeText2() {
+    let txt = ``;
+    return Promise.all([getText1(), getText2(), getText3()]).then((res) =>
+        res.reduce((acc, cur) => {
+            return acc + cur;
+        }),
+    );
+}
+
+async function mergeText3() {
+    let txt = ``;
+    return await Promise.all([getText1(), getText2(), getText3()]).then((res) =>
+        res.reduce((acc, cur) => {
+            return acc + cur;
+        }),
+    );
+}
+
+mergeText1().then((res) => console.log(res));
+mergeText2().then((res) => console.log(res));
+mergeText2().then((res) => console.log(res));
